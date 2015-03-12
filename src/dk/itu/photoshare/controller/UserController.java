e32e0ba521a107791a9eb68c485db950c81138a9
jonaslomholdt <jonaslomholdt@gmail.com>
2015-03-12 17:57:46 +0100

    Changed Servlet Mapping to match updated name

--- src/dk/itu/photoshare/controller/UserController.java
+++ src/dk/itu/photoshare/controller/UserController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class NameController
  */
-@WebServlet("/NameController")
+@WebServlet("/UserController")
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
