71a792bb5fcace1b4a5f8494c6ddebd742fb5a43
jonaslomholdt <jonaslomholdt@gmail.com>
2015-03-13 15:55:42 +0100

    Added User model

--- src/dk/itu/photoshare/model/User.java
+++ src/dk/itu/photoshare/model/User.java
@@ -1,5 +1,28 @@
 package dk.itu.photoshare.model;
 
 public class User {
-
+	
+	private int id;
+	private String username;
+	
+	public User(int id, String username) {
+		this.id = id;
+		this.username = username;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
