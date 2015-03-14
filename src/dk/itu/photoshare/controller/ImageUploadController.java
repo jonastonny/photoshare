8e9061695babdeeaf1f73cf8fac40fe9190f466c
Niels Dawartz <nielsdaw@gmail.com>
2015-03-14 12:33:21 +0100

    added ImageUploadController

--- src/dk/itu/photoshare/controller/ImageUploadController.java
+++ src/dk/itu/photoshare/controller/ImageUploadController.java
@@ -1,6 +1,8 @@
 package dk.itu.photoshare.controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,13 +15,13 @@ import javax.servlet.http.Part;
  * Servlet implementation class ImageController
  */
 @WebServlet("/ImageController")
-public class ImageController extends HttpServlet {
+public class ImageUploadController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ImageController() {
+    public ImageUploadController() {
         super();
     }
 
@@ -34,18 +36,32 @@ public class ImageController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("her");
 		String description = request.getParameter("description");
-		Part image = request.getPart("image");
+		String user_id = "1";
+		Part image = request.getPart("image"); 	// Retrieves <input type="image" name="image">
 		
 		if(image != null){
 			System.out.println("image: " + image.getName());
 			System.out.println("size: " + image.getSize());
 			System.out.println("image type: " + image.getContentType());
 		}
-		
-		
-		
-		
+	    String imageName = getFileName(image);
+	    InputStream fileContent = image.getInputStream();
+	    
+	    
+
 	}
+	
+	private static String getFileName(Part image) {
+	    for (String cd : image.getHeader("content-disposition").split(";")) {
+	        if (cd.trim().startsWith("filename")) {
+	            String fileName = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+	            return fileName.substring(fileName.lastIndexOf('/') + 1).substring(fileName.lastIndexOf('\\') + 1); // MSIE fix.
+	        }
+	    }
+	    return null;
+	}
+	
 
 }
