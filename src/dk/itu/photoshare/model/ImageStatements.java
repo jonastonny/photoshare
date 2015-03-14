337f2cd4cad29a8feebd40ac0cba276a7358eb48
Niels Dawartz <nielsdaw@gmail.com>
2015-03-14 15:50:22 +0100

    image crap

--- src/dk/itu/photoshare/model/ImageStatements.java
+++ src/dk/itu/photoshare/model/ImageStatements.java
@@ -1,5 +1,10 @@
 package dk.itu.photoshare.model;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -26,4 +31,25 @@ public class ImageStatements {
 		}
 		return "No image for you, sir";
 	}
+	
+	
+	public static void uploadImgToDB(String imgURL, String description, String userID){
+		
+	}
+	
+	public static void uploadImgToServer(String imageName, String username, InputStream imageContent){
+		String upload = "/images/";
+		File file = new File(upload, (username + imageName));
+		System.out.println(file.toPath());
+		System.out.println(imageContent.toString());
+
+
+		try (InputStream input = imageContent) {
+				Files.copy(input, file.toPath());	// copy the content of the imageContent to the hardcoded path
+				System.out.println("File succesfully uploaded to server");
+			} catch (IOException e) {
+				System.out.println(e);
+		}
+	}
+	
 }
