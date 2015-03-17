ddbcfe89851cc864807166dddcbecc4f2f733a3e
jonaslomholdt <jonaslomholdt@gmail.com>
2015-03-17 12:55:02 +0100

    videre

--- src/dk/itu/photoshare/controller/ImageUploadController.java
+++ src/dk/itu/photoshare/controller/ImageUploadController.java
@@ -3,6 +3,7 @@ package dk.itu.photoshare.controller;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.File;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -10,8 +11,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
+
 import dk.itu.photoshare.model.ImageStatements;
+import dk.itu.photoshare.model.User;
 
 /**
  * Servlet implementation class ImageController
@@ -33,6 +37,15 @@ public class ImageUploadController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession s = request.getSession();
+		User user = (User) s.getAttribute("user");
+		
+		if(user == null){
+			request.setAttribute("error", "You need to login to upload idiot!");
+			RequestDispatcher view = request.getRequestDispatcher("views/index.jsp");
+			view.forward(request, response);
+			return;
+		}
 		RequestDispatcher view = request.getRequestDispatcher("views/images/upload.jsp");
 		view.forward(request, response);
 	}
@@ -41,16 +54,24 @@ public class ImageUploadController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession s = request.getSession();
+		User user = (User) s.getAttribute("user");
+		if(user == null){
+			request.setAttribute("error", "You need to login to upload idiot!");
+			RequestDispatcher view = request.getRequestDispatcher("views/index.jsp");
+			view.forward(request, response);
+			return;
+		}
+		
+		
 		String description = request.getParameter("description");
-		String user_id = "1";	// hardcoded - will be set by sessionParameter
-		String username = "test"; // hardcoded - will be set by sessionParameter
 		Part image = request.getPart("image");	// get image
 
 		if(!(image.getSize() <= 0)){
 		    //String imgName = getImageName(image);
 			InputStream imageContent = image.getInputStream();
 			ImageStatements upload = new ImageStatements();
-			upload.uploadImgToDB(imageContent, description, user_id);
+			upload.uploadImgToDB(imageContent, description, Integer.toString(user.getId()));
 			RequestDispatcher view = request.getRequestDispatcher("views/images/image.jsp");
 			view.forward(request, response);
 		}
